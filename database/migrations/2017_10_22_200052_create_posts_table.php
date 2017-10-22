a2327d4575fca12cd93659ead944198aebcca7a5
Kotique Andreev <kotique@protonmail.com>
2017-10-22 23:15:04 +0300

    Create DB models

--- database/migrations/2017_10_22_200052_create_posts_table.php
+++ database/migrations/2017_10_22_200052_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
-            $table->string('password');
+            $table->string('body');
+            $table->integer('theme_id');
+            $table->integer('answers_to_post_id');
+            $table->integer('author_id');
             $table->timestamps();
         });
     }
@@ -28,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
